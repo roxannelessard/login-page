e5699dd5497f6feab51386dbe25fb75e59fa3b1a
roxanne lessard <me@rlessard.com>
2021-12-13 16:55:55 -0500

    feat: adds useEffect for setLoggedIn to true

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,9 +7,20 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+      // the use of useEffect will allow the user to refresh the page and still be logged in!
+      // we ran the code here to avoid an infinite loop
+      const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+      if (storedUserLoggedInInformation === "1") {
+        setIsLoggedIn(true);
+      }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
